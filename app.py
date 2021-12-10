7a3525f8c693cee1686746309188c46d562f8953
febrel <febreluis06@gmail.com>
2021-12-10 08:24:37 -0500

    Subida de proyecto

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, app, render_template, request, flash
+import random
+
+app = Flask(__name__)
+
+app.secret_key = "manbearpig_MUDMAN888"
+
+
+@app.route('/')
+def index():
+    flash('')
+    return render_template('index.html')
+
+
+@app.route('/greet', methods=['POST', 'GET'])
+def greet():
+    # Arreglos propuestos
+    alpha_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alpha_lower = 'abcdefghijklmnopqrstuvwxyz'
+    numbers = '0123456789'
+    caracters = '!#$[%&/(<)=?¿¡*}+-{],._>'
+
+
+    base = alpha_upper + alpha_lower + numbers + caracters
+    longitud = 14
+
+    muestra = random.sample(base, longitud)
+    password = ''.join(muestra)
+
+    flash(password)
+    return render_template('index.html')
+
+
+
+# for _ in range(10):
+
+#     muestra = random.sample(base, longitud)
+#     password = ''.join(muestra)
+#     print(password)
